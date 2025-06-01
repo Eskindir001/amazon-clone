d4df57a38b901105d887742d46102aedd0a3bfff
Eska12345678 <94687142+Eska12345678@users.noreply.github.com>
2025-06-01 20:28:11 +0300

    progress

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { DataProvider } from './Components/DataProvider/DataProvider.jsx'
+import {reducer,initialState} from './Utility/reducer.js'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DataProvider initialState={initialState} reducer={reducer}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DataProvider>
+  </StrictMode>
+);
